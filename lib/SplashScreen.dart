dad654d8b883e4530d92e8ed6aa83a2c8d6f4987
waqarmian69 <waqarmian69@gmail.com>
2020-12-05 03:55:05 -0800

    simple.dart AAdded

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,7 +1,6 @@
 import 'package:fa17_bse_030_2nd_sessional/main.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-
 void main(){
   runApp(new MaterialApp(
     home: new MyApp(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:fa17_bse_030_2nd_sessional/Navigation.dart';
-
+import 'package:fa17_bse_030_2nd_sessional/SplashScreen.dart';
+import 'dart:async';
+import 'Simple.dart';
+// import 'Hard.dart';
 void main() {
-  runApp(Home());
+  runApp(SplashScreen());
 }
 
-class Home extends StatelessWidget {
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dice App',
-      home: HomePage(),
+      title: 'Splash Screen',
       theme: ThemeData(
-          primarySwatch: Colors.red
+        primarySwatch: Colors.green,
       ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SimpleHard())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black87,
+      child: SplashScreen(),
+    );
+  }
+}
+//icon Changed
+
+class SimpleHard extends StatefulWidget {
+  @override
+  _SimpleHardState createState() => _SimpleHardState();
+}
+
+class _SimpleHardState extends State<SimpleHard> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => Simple()));
+                },
+                child: Text(
+                  'Simple',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+                width: 400.0,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => Hard()));
+                },
+                child: Text(
+                  'Hard',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
